abd78bae26b50ecd8a8d1326533712a1d5f029c2
Mostafa Salama <105205855+MostaSalama@users.noreply.github.com>
2022-09-14 03:04:38 +0200

    ""

--- app/src/main/java/com/example/locationpractise/MainActivity.kt
+++ app/src/main/java/com/example/locationpractise/MainActivity.kt
@@ -1,30 +1,78 @@
 package com.example.locationpractise
 
-import android.content.DialogInterface
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationRequest
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.example.locationpractise.Base.BaseActivity
-import java.util.jar.Manifest
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationRequest.create
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity() , OnMapReadyCallback {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+     var googleMap: GoogleMap? = null
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContentView(R.layout.activity_main)
 
-        if(!isGPSPermissionAllowed()){
+        if(isGPSPermissionAllowed()){
+            getUserLocation()
+        }else{
             requestPermission()
         }
 
     }
 
-    val requestGPSPermissionLauncher =
+    var userLocation : Location?=null
+    var userMarker : Marker?=null
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        drawUserMarkerOnMap()
+    }
+
+    fun drawUserMarkerOnMap(){
+        val latLng = LatLng(userLocation?.latitude?:0.0,userLocation?.longitude?:0.0)
+
+        if (userMarker==null){
+            var markerOptions = MarkerOptions()
+            markerOptions.position(latLng)
+            markerOptions.title("Current Location")
+            userMarker = googleMap?.addMarker(markerOptions)
+
+        }else
+            userMarker?.position = latLng
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16.0f))
+    }
+
+    private val requestGPSPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
@@ -55,6 +103,7 @@ class MainActivity : BaseActivity() {
         }else{
             requestGPSPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
+
         }
     }
 
@@ -63,12 +112,79 @@ class MainActivity : BaseActivity() {
 
 
     fun isGPSPermissionAllowed():Boolean{
+
         return ContextCompat.checkSelfPermission(this,
         android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
+    val locationCallback:LocationCallback = object :LocationCallback(){
+
+        override fun onLocationResult(location: LocationResult) {
+
+            for (location in location.locations){
+                Log.e("location updated",""+location.latitude + " "+location.longitude)
+                userLocation = location
+                drawUserMarkerOnMap()
+            }
+        }
+    }
+
+
+    val locationRequest = create().apply {
+            interval = 10000
+            fastestInterval = 3000
+            priority = LocationRequest.QUALITY_HIGH_ACCURACY
+    }
+
+    val REQUEST_CHECK_SETTINGS = 120
+    @SuppressLint("MissingPermission")
     fun getUserLocation(){
-        Toast.makeText(this,"we can access user location",Toast.LENGTH_LONG).show()
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback,
+                Looper.getMainLooper())
+            Toast.makeText(this,"Location is enabled !",Toast.LENGTH_LONG).show()
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity,
+                        REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+
+
+
+
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CHECK_SETTINGS){
+                if(requestCode== RESULT_OK){
+                    getUserLocation()
+                }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+
 }
